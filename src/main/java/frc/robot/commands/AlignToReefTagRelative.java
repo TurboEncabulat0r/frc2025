086c93134878899f78e07dc47cb2fae572f98d2e
Benjamin <benjaminro21@gmail.com>
2025-03-12 15:19:50 -0500

    Add limelight helpers and Align Command

---
+++ src/main/java/frc/robot/commands/AlignToReefTagRelative.java
@@ -0,0 +1,113 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.LinearVelocityUnit;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import edu.wpi.first.math.util.Units;
+
+public class AlignToReefTagRelative extends Command {
+  private PIDController xController, yController, rotController;
+  private boolean isRightScore;
+  private Timer dontSeeTagTimer, stopTimer;
+  private CommandSwerveDrivetrain drivebase;
+  private double tagID = -1;
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+  private final SwerveRequest.RobotCentric driveRC = new SwerveRequest.RobotCentric()
+  .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+  .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+  public AlignToReefTagRelative(boolean isRightScore, CommandSwerveDrivetrain drivebase) {
+    xController = new PIDController(Constants.X_REEF_ALIGNMENT_P, 0.0, 0);  // Vertical movement
+    yController = new PIDController(Constants.Y_REEF_ALIGNMENT_P, 0.0, 0);  // Horitontal movement
+    rotController = new PIDController(Constants.ROT_REEF_ALIGNMENT_P, 0, 0);  // Rotation
+    this.isRightScore = isRightScore;
+    this.drivebase = drivebase;
+    addRequirements(drivebase);
+  }
+
+  @Override
+  public void initialize() {
+    this.stopTimer = new Timer();
+    this.stopTimer.start();
+    this.dontSeeTagTimer = new Timer();
+    this.dontSeeTagTimer.start();
+
+    rotController.setSetpoint(Constants.ROT_SETPOINT_REEF_ALIGNMENT);
+    rotController.setTolerance(Constants.ROT_TOLERANCE_REEF_ALIGNMENT);
+
+    xController.setSetpoint(Constants.X_SETPOINT_REEF_ALIGNMENT);
+    xController.setTolerance(Constants.X_TOLERANCE_REEF_ALIGNMENT);
+
+    yController.setSetpoint(isRightScore ? Constants.Y_SETPOINT_REEF_ALIGNMENT : -Constants.Y_SETPOINT_REEF_ALIGNMENT);
+    yController.setTolerance(Constants.Y_TOLERANCE_REEF_ALIGNMENT);
+
+    tagID = LimelightHelpers.getFiducialID("");
+  }
+
+  @Override
+  public void execute() {
+    if (LimelightHelpers.getTV("") && LimelightHelpers.getFiducialID("") == tagID) {
+      this.dontSeeTagTimer.reset();
+
+      double[] postions = LimelightHelpers.getBotPose_TargetSpace("");
+      SmartDashboard.putNumber("x", postions[2]);
+
+      double xSpeed = xController.calculate(postions[2]);
+      SmartDashboard.putNumber("xspee", xSpeed);
+      double ySpeed = -yController.calculate(postions[0]);
+      double rotValue = -rotController.calculate(postions[4]);
+      SwerveRequest.RobotCentric m_drive;
+      
+      drivebase.setControl(m_drive
+                .withVelocityX(xSpeed) // Drive forward with negative Y(forward)
+                .withVelocityY(ySpeed) // Drive left with negative X (left)
+                .withRotationalRate(rotValue)); // Drive counterclockwise with negative X (left)
+
+      if (!rotController.atSetpoint() ||
+          !yController.atSetpoint() ||
+          !xController.atSetpoint()) {
+        stopTimer.reset();
+      }
+    } else {
+      
+      
+    }
+
+    SmartDashboard.putNumber("poseValidTimer", stopTimer.get());
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    drivebase.drive(new Translation2d(), 0, false);
+  }
+
+  @Override
+  public boolean isFinished() {
+    // Requires the robot to stay in the correct position for 0.3 seconds, as long as it gets a tag in the camera
+    return this.dontSeeTagTimer.hasElapsed(Constants.DONT_SEE_TAG_WAIT_TIME) ||
+        stopTimer.hasElapsed(Constants.POSE_VALIDATION_TIME);
+  }
+}
